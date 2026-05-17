88942b73ec8dfe256d3bdd72e925a6927073201d
shyweeds <2149934895@qq.com>
2026-05-17 13:41:01 +0800

    coalesce(): ok

--- mm.c
+++ mm.c
@@ -118,7 +118,33 @@ static void *coalesce(char *bp){
   size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
   size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
   size_t size = GET_SIZE(HDRP(bp));
-  return NULL;
+
+  if(prev_alloc && next_alloc) /*case 1*/
+  {
+    return bp;
+  }
+  else if(prev_alloc && !next_alloc) /*case 2*/
+  {
+    size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+  }
+  else if(!prev_alloc && next_alloc) /*case 3*/
+  {
+    size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+    bp = PREV_BLKP(bp);
+  }
+  else if(!prev_alloc && !next_alloc) /*case 4*/
+  {
+    size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
+      GET_SIZE(HDRP(NEXT_BLKP(bp)));
+    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+    PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+    bp = PREV_BLKP(bp);
+  }
+  return bp;
 }
 
 /* 
