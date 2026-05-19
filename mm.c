b678faea9351da6418d0a2dd91b2f83a73e6988e
shyweeds <2149934895@qq.com>
2026-05-20 00:36:22 +0800

    fix more bugs about find_fit()...

--- mm.c
+++ mm.c
@@ -291,7 +291,7 @@ void *mm_malloc(size_t size)
     asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE);/*向上取整技巧*/
 
   /*Search the free list for a fit*/
-  if((bp = find_fit(asize)) != NULL)
+  if((bp = find_fit(asize)))
   {
   /******************************************************/
   dump_heap(heap_listp);
@@ -325,33 +325,34 @@ void *mm_malloc(size_t size)
 }
 static void *find_fit(size_t asize)
 {
-  char *bp;
-  for(bp = heap_listp; GET_SIZE(HDRP(bp)) > asize; bp = NEXT_BLKP(bp));
-  if(GET_SIZE(HDRP(bp)) > asize)
-    return bp;
-  else
+  char *bp = heap_listp;
+  while(GET_SIZE(HDRP(bp)) < asize && GET_SIZE(HDRP(bp)) != 0)
+  {
+    bp = NEXT_BLKP(bp);
+  }
+  if(GET_SIZE(HDRP(bp)) == 0)
     return NULL;
+  else
+    return bp;
 }
 static void place(void *bp, size_t asize)
 {
   size_t left_size = GET_SIZE(bp) - asize;
 
-  PUT(HDRP(bp), PACK(asize, 1));
-  PUT(FTRP(bp), PACK(asize, 1));
-//   if(left_size < MIN_BLOCK_SIZE) //剩余空间小于最小块大小 
-//   { 
-//     PUT(HDRP(bp), PACK(asize, 1));
-//     PUT(FTRP(bp), PACK(asize, 1));
-//   }
-//   else
-//   {  
-//     PUT(HDRP(bp), PACK(asize, 1));
-//     PUT(FTRP(bp), PACK(asize, 1));
-// 
-//     //把剩下的块单独变成一个空闲块
-//     PUT(HDRP(NEXT_BLKP(bp)), PACK(left_size, 0));
-//     PUT(FTRP(NEXT_BLKP(bp)), PACK(left_size, 0));
-//   }
+  if(left_size >= MIN_BLOCK_SIZE) //剩余空间大于等于最小块大小 
+  { 
+    PUT(HDRP(bp),             PACK(asize, 1));
+    PUT(FTRP(bp) - left_size, PACK(asize, 1));
+
+    //把剩下的块单独变成一个空闲块
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(left_size, 0));
+    PUT(FTRP(NEXT_BLKP(bp)), PACK(left_size, 0));
+  }
+  else //不拆分,产生内部碎片
+  {  
+    PUT(HDRP(bp), PACK(asize, 1));
+    PUT(FTRP(bp), PACK(asize, 1));
+  }
 }
 
 /*
